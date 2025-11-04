0efaf1d868b0745c7c32dc2c4da23e87f8821365
Jacob <jackan202@gmail.com>
2025-11-04 14:47:09 +0100

    Funktionalitet för att lägga till, ta bort, och hitta olika anställda i registriet

--- PersonalRegister/PersonalRegister.cs
+++ PersonalRegister/PersonalRegister.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,16 +17,26 @@ namespace PersonalRegister
             register = new();
         }
 
-        public void Add(Personal p)
+        public void LäggTill(Personal p)
         {
             register.Add(p);
             AntalAnställda++;
         }
 
-        public void Delete(string id)
+        public void Radera(string id)
         {
+            Personal? p = register.Find(p => p.Id == id);
+            if (p is null) {
+                Console.WriteLine($"Anställd med id \"{id}\" kunde inte hittas.");
+                return;
+            }
+            register.Remove(p);
             AntalAnställda--;
         }
+
+        public Personal? HittaMedId(string id) => register.First(p => p.Id == id);
+        public List<Personal> HittaMedNamnFörNamn(string förNamn) => register.Where(p => p.FörNamn == förNamn).ToList();
+        public List<Personal> HittaMedNamnEfterNamn(string efterNamn) => register.Where(p => p.EfterNamn == efterNamn).ToList();
 
         public void PrintRegister()
         {
