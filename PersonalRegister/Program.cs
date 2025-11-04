2f8b2c95847d0b1a74ebf714488e7f387066652f
Jacob <jackan202@gmail.com>
2025-11-04 15:00:44 +0100

    Påbörjat meny

--- PersonalRegister/Program.cs
+++ PersonalRegister/Program.cs
@@ -4,8 +4,34 @@
     {
         static void Main(string[] args)
         {
-            var a = new Personal("a", "b", 12);
-            a.SkrivPersonalInfo();
+            string input = "";
+            do {
+                Console.WriteLine("Personalregister");
+                Console.WriteLine("1. Lägg till ny anställd");
+                Console.WriteLine("2. Hitta en viss anställd");
+                Console.WriteLine("3. Se antalet anställda");
+                Console.WriteLine("4. Ta bort en viss anställd");
+                Console.WriteLine("E. Avsluta Applikationen");
+                input = Console.ReadLine() ?? "";
+                input = input.ToUpper();
+
+                switch (input) {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    default:
+                        Console.WriteLine("Invalid inputm, please try again");
+                        continue;
+                    case "E":
+                        break;
+                }
+
+            } while (input != "E");
         }
     }
 }
